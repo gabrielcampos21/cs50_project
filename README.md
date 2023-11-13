32c3b67eec03531a4eaa6c8b79e692b3d32a1d79
gabrielcampos21 <gabrielcampos2143@gmail.com>
2023-11-13 12:26:17 -0300

    added dataset updater

--- README.md
+++ README.md
@@ -24,10 +24,12 @@ I decided to do this project because sometimes I find it hard to search and disc
 
 
 ## Project Files
--  steam-co-op-game-recommender.ipynb -> this is the downloaded Kaggle notebook, which:
-    - reads the dataset into memory, drops unnecessary columns and modifies the ones needed and filters non coop games out of the dataframe.
-    -  Then it does the calculation of how relevant a word is to a text in the games 'About the game' column and generates a matrix with a measure of how similar two elements are, indicating how similar one game is to another.
-    -  After that, these variables are saved into pickle files to be read by other python files.
+-  dataset_updater.ipynb
+    - Uses kaggles api to download and extract the dataset.
+    - Reads the dataset into memory, drops unnecessary columns and modifies the ones needed and filters non coop games out of the dataframe.
+    - Then it does the calculation of how relevant a word is to a text in the games 'About the game' column and generates a matrix with a measure of how similar two elements are, indicating how similar one game is to another.
+    - After that, these variables are saved into pickle files to be read by other python files.
+- dataset_updater.py -> this does the same thing as the jupyter notebook (dataset_updater.ipynb), but without demonstrations. It's another option to update the dataset.
 -  app.py -> Simple python and flask routes controller with 3 routes:
     - '/' (GET) which renders the index page.
     - '/search' (POST) which runs the recommendation function and renders the game recommendations list.
@@ -36,10 +38,13 @@ I decided to do this project because sometimes I find it hard to search and disc
     - complete(provides the list of game titles based on what the user is writting).
     - get_recommendations(returns similar games based on the game name and filter options).
 
-The point of having these pickle files is that I find it more convenient to download the dataset and generate the similarity matrixes on Kaggle, and then downloading them as files to be read by the website recommender python file. This way, I can choose when I want to update the games data and can use Kaggle's computer resources to do that faster.
+The point of having these pickle files is that I find it more convenient to download the dataset and generate the similarity matrixes on demand. This way, I can choose when I want to update the games data.
 
-The Kaggle python notebook also contains a few examples of how the recommendation can be implemented and also how the search function works.
 
-## Demonstration
-[Youtube video](https://www.youtube.com/)
-
+## Usage
+- ### Configure Kaggle API credentials:
+  - [Kaggle API Documentation](https://github.com/Kaggle/kaggle-api): To use the Kaggle API, sign up for a Kaggle account at https://www.kaggle.com. Then go to the 'Account' tab of your user profile (https://www.kaggle.com/<username>/account) and select 'Create API Token'. This will trigger the download of kaggle.json, a file containing your API credentials. Place this file in the location ~/.kaggle/kaggle.json (on Windows in the location C:\Users\<Windows-username>\.kaggle\kaggle.json - you can check the exact location, sans drive, with echo %HOMEPATH%).
+- clone repository
+- pip install -r requirements.txt
+- python dataset_updater.py
+- python app.py  
---
+++ dataset_updater.py
@@ -0,0 +1,92 @@
+import pickle
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+import kaggle
+import os
+import numpy as np
+import pandas as pd
+
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files(
+    'fronkongames/steam-games-dataset', path='data', unzip=True)
+
+# Removing unnecessary files
+os.remove('data/games.json')
+
+# Reading the dataset
+df = pd.read_csv('data/games.csv')
+
+# Adding a 'PositiveRate' column
+df['PositiveRate'] = df['Positive'] / (df['Positive'] + df['Negative'])
+
+# Dropping null values and games which don't support English language
+df = df.drop(df[df['About the game'].isna()].index)
+df = df.drop(df[df['Categories'].isna()].index)
+df = df.drop(df[df['Genres'].isna()].index)
+df = df.drop(df[df['Tags'].isna()].index)
+df = df.drop(df[~df['Supported languages'].str.contains('English')].index)
+
+# Limiting the 'About the game' text to 1800 characters (so it won't use too much ram)
+# This is optional
+# df['About the game'] = df['About the game'].apply(lambda x: x[:1800] if len(x) > 1800 else x)
+
+# Keeping only the year in the 'Release date' label
+df['Release date'] = df['Release date'].str[-4:]
+
+# Dropping labels that won't be used
+labels_to_drop = ['Reviews', 'Peak CCU',
+                  'Required age', 'DLC count',
+                  'Full audio languages',
+                  'Header image', 'Website', 'Support url', 'Support email', 'Windows',
+                  'Mac', 'Linux', 'Metacritic score', 'Metacritic url', 'User score',
+                  'Score rank', 'Achievements', 'Recommendations',
+                  'Notes', 'Average playtime forever', 'Average playtime two weeks',
+                  'Median playtime forever', 'Median playtime two weeks', 'Developers',
+                  'Publishers', 'Screenshots', 'Movies']
+
+for label in labels_to_drop:
+    df.drop(label, axis=1, inplace=True)
+
+# Filtering non coop games
+df = df[df['Categories'].str.contains('Co-op')]
+df = df.reset_index(drop=True)
+
+
+# TF-IDF(Term Frequency Inverse Document Frequency):
+# It can be defined as the calculation of how relevant a word is to a text
+# Define a TF-IDF Vectorizer Object. Remove all english stop words such as 'the', 'a'
+tfidf = TfidfVectorizer(stop_words='english')
+
+# Fits the TF-IDF vectorizer on the 'About the game' column of the df DataFrame,
+# which means it learns the vocabulary of the corpus and calculates the term frequencies and inverse document frequencies for each term.
+tfidf_matrix = tfidf.fit_transform(df['About the game'])
+
+# Compute the cosine similarity matrix: a square matrix where each element represents the cosine similarity between two documents.
+# It's a measure of how similar they are, with a higher cosine similarity indicating greater similarity.
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+# Construct map of indices and game titles
+indices = pd.Series(df.index, index=df['Name']).drop_duplicates()
+
+
+# Define the filename for the pickle file
+PICKLE_FILENAME = 'pickle_files/cosine_similarity_matrix.pkl'
+
+# Save the cosine_sim matrix to a pickle file
+with open(PICKLE_FILENAME, 'wb') as pickle_file:
+    pickle.dump(cosine_sim, pickle_file)
+
+PICKLE_FILENAME = 'pickle_files/df.pkl'
+
+with open(PICKLE_FILENAME, 'wb') as pickle_file:
+    pickle.dump(df, pickle_file)
+
+PICKLE_FILENAME = 'pickle_files/indices.pkl'
+
+with open(PICKLE_FILENAME, 'wb') as pickle_file:
+    pickle.dump(indices, pickle_file)
+
+
+os.remove('data/games.csv')
+
+print('Done!')
